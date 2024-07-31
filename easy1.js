6828b4bd19ad3ec5c3b936a6782e6406e85567e2
kk <taikang@pisolutions.tech>
2024-07-31 15:33:48 +0800

    chore: update workflow

--- easy1.js
+++ easy1.js
@@ -1,4 +1,4 @@
-// 題目描述：給定一個數字陣列，請寫一個函數來移除陣列中的重複元素，返回一個新的陣列。
+// 題目描述：sssss，請寫一個函數來移除陣列中的重複元素，返回一個新的陣列。
 const arr = [1, 2, 2, 3, 4, 4, 5];
 console.log(removeDuplicates(arr)); // 輸出: [1, 2, 3, 4, 5]
 
@@ -9,4 +9,4 @@ console.log(removeDuplicates(arr)); // 輸出: [1, 2, 3, 4, 5]
 function removeDuplicates(array) {
     return Array.from(new Set(array));
 }
-// 46666
+// 77777
